0e1455852838133cc9c46658d28d39d44c41f792
Damien Guard <damieng@gmail.com>
2013-09-23 16:39:18 -0700

    Add first xunit test and solution.

---
+++ Source/IQToolkit.Test.ElasticSearch/TypeSystem/ElasticSearchQueryTypeTests.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Tier 3 Inc. All rights reserved.
+// This source code is made available under the terms of the Microsoft Public License (MS-PL)
+
+using IQToolkit.Data.ElasticSearch.TypeSystem;
+using Xunit;
+
+namespace IQToolkit.Test.ElasticSearch.TypeSystem
+{
+    public class ElasticSearchQueryTypeTests
+    {
+        [Fact]
+        public void ConstructorSetsProperties()
+        {
+            const bool notNull = true;
+            const int length = 1234;
+            const short precision = 18; const short scale = 9;
+
+            var actual = new ElasticSearchQueryType(notNull, length, precision, scale);
+
+            Assert.Equal(notNull, actual.NotNull);
+            Assert.Equal(length, actual.Length);
+            Assert.Equal(precision, actual.Precision); 
+            Assert.Equal(scale, actual.Scale);
+        }
+    }
+}
