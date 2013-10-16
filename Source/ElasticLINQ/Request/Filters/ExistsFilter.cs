95afee33ed66242c1196fa120f90b0a83555518f
Damien Guard <damieng@gmail.com>
2013-10-16 14:55:08 -0700

    HasValue to Exists filter mapping.

---
+++ Source/ElasticLINQ/Request/Filters/ExistsFilter.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Tier 3 Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. 
+
+using System;
+
+namespace ElasticLinq.Request.Filters
+{
+    internal class ExistsFilter : IFilter
+    {
+        private readonly string field;
+
+        public ExistsFilter(string field)
+        {
+            this.field = field;
+        }
+
+        public string Field
+        {
+            get { return field; }
+        }
+
+        public string Name
+        {
+            get { return "exists"; }
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} [{1}]", Name, Field);
+        }
+    }
+}
---
+++ Source/TestConsoleApp/Utility/Dump.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace TestConsoleApp.Utility
+{
+    public static class Dump
+    {
+        public static void Query<T>(IQueryable<T> query)
+        {
+            Console.WriteLine(query);
+
+            Console.WriteLine("\nResults:");
+
+            foreach (var item in query)
+                Dump.Object(item);
+        }
+
+        public static void Object(object value)
+        {
+            Console.WriteLine();
+
+            foreach (var property in value.GetType().GetProperties())
+                Console.WriteLine(property.Name + " : " + property.GetValue(value));
+
+            foreach (var field in value.GetType().GetFields())
+                Console.WriteLine(field.Name + " : " + field.GetValue(value));
+        }
+    }
+}
