8a73a6877048bb7f9e100341d46e0ce49738be39
Brad Wilson <dotnetguy@gmail.com>
2014-06-13 12:22:05 -0700

    Update to version 1.1.0-beta1

--- Source/GlobalAssemblyInfo.cs
+++ Source/GlobalAssemblyInfo.cs
@@ -2,8 +2,8 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("ElasticLINQ")]
-[assembly: AssemblyCopyright("Copyright (C) 2013 CenturyLink Cloud Inc.")]
+[assembly: AssemblyCopyright("Copyright (C) 2013-4 CenturyLink Cloud Inc.")]
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
