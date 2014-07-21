ba351d9d02e405af394566b73df1fdfa4ca30abc
Damien Guard <damien@envytech.co.uk>
2014-07-21 11:04:12 -0700

    Stop FacetExpressionVisitor visiting so much of the expression tree.

--- Source/ElasticLINQ/Request/Visitors/FacetExpressionVisitor.cs
+++ Source/ElasticLINQ/Request/Visitors/FacetExpressionVisitor.cs
@@ -146,8 +146,8 @@ namespace ElasticLinq.Request.Visitors
                 if (aggregateMemberOperations.TryGetValue(m.Method.Name, out operation) && m.Arguments.Count == 2)
                     return VisitAggregateMemberOperation(m.Arguments[1], operation, m.Method.ReturnType);
             }
-
-            return base.VisitMethodCall(m);
+            
+            return m; // Do not base.VisitMethodCall as we don't want to examine the whole tree
         }
 
         private Expression VisitTake(Expression source, Expression takeExpression)
