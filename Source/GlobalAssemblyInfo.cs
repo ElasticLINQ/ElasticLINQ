1d2adbcba5f51f8bf2d8369dca576519022fbdf0
Damien Guard <damien@envytech.co.uk>
2014-08-27 12:57:29 -0700

    Correct copyright notices.

--- Source/GlobalAssemblyInfo.cs
+++ Source/GlobalAssemblyInfo.cs
@@ -2,7 +2,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("ElasticLINQ")]
-[assembly: AssemblyCopyright("Copyright (C) 2013-4 CenturyLink Cloud Inc.")]
+[assembly: AssemblyCopyright("Copyright (C) 2013-2014 CenturyLink, Inc.")]
 
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.1.0.0")]
