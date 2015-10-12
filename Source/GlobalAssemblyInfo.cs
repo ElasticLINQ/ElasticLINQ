43569217d758add388dce1d6eb0d7f0d05cb7979
Damien Guard <damieng@gmail.com>
2015-10-12 13:09:31 -0700

    Update package for ElasticLINQ 1.4.1

--- Source/GlobalAssemblyInfo.cs
+++ Source/GlobalAssemblyInfo.cs
@@ -1,9 +1,9 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyProduct("ElasticLINQ.ElasticsearchNet")]
+[assembly: AssemblyProduct("ElasticLINQ")]
 [assembly: AssemblyCopyright("Copyright (C) 2013-2015 CenturyLink, Inc.")]
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.4.0.0")]
-[assembly: AssemblyFileVersion("1.4.0.0")]
+[assembly: AssemblyVersion("1.4.1.0")]
+[assembly: AssemblyFileVersion("1.4.1.0")]
