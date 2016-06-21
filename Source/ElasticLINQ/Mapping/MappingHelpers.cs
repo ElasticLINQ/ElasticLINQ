431f239e23302292ef9253e745ac163711cc292d
Damien Guard <damieng@gmail.com>
2016-06-21 11:19:30 -0700

    Make MappingHelpers culture aware

--- Source/ElasticLINQ/Mapping/MappingHelpers.cs
+++ Source/ElasticLINQ/Mapping/MappingHelpers.cs
@@ -18,14 +18,14 @@ namespace ElasticLinq.Mapping
         /// Convert a string to camel-case.
         /// </summary>
         /// <param name="value">Input string to be camel-cased.</param>
-        /// <param name="culture">CultureInfo to be used to lower-case first character.</param>
+        /// <param name="culture">CultureInfo to be used to lower-case first word.</param>
         /// <returns>String that has been converted to camel-case.</returns>
         public static string ToCamelCase(this string value, CultureInfo culture)
         {
-            Argument.EnsureNotNull("value", value);
+            Argument.EnsureNotNull(nameof(value), value);
 
             var words = Regex.Split(value, "(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])");
-            return string.Concat(words.First().ToLowerInvariant(), string.Concat(words.Skip(1)));
+            return string.Concat(culture.TextInfo.ToLower(words.First()), string.Concat(words.Skip(1)));
         }
 
         /// <summary>
@@ -39,11 +39,11 @@ namespace ElasticLinq.Mapping
         /// </remarks>
         public static string ToPlural(this string value, CultureInfo culture)
         {
-            Argument.EnsureNotNull("value", value);
+            Argument.EnsureNotNull(nameof(value), value);
 
             return value.Length < 1
                 ? value
-                : value + (value.EndsWith("s", StringComparison.Ordinal) ? "" : "s");
+                : value + (culture.CompareInfo.IsSuffix(value, "s", CompareOptions.IgnoreCase) ? "" : "s");
         }
 
         /// <summary>
@@ -58,8 +58,8 @@ namespace ElasticLinq.Mapping
         public static PropertyInfo GetTypeSelectionProperty(Type type)
         {
             var typeInfo = type.GetTypeInfo();
-            return typeInfo.DeclaredProperties.FirstOrDefault(BasicTypeSelectionPropertyCriteria)
-                   ?? RequiredProperty(typeInfo);
+            return typeInfo.DeclaredProperties.FirstOrDefault(basicTypeSelectionPropertyCriteria)
+                   ?? requiredProperty(typeInfo);
         }
 
         /// <summary>
@@ -68,7 +68,7 @@ namespace ElasticLinq.Mapping
         /// <remarks>
         /// A type selection property should be a public read/write instance property that is not generic.
         /// </remarks>
-        public static Func<PropertyInfo, bool> BasicTypeSelectionPropertyCriteria = p =>
+        static readonly Func<PropertyInfo, bool> basicTypeSelectionPropertyCriteria = p =>
             p.CanRead && p.CanWrite && p.GetMethod.IsPublic && !p.GetMethod.IsStatic &&
             p.PropertyType.GetTypeInfo().IsValueType && !p.PropertyType.GetTypeInfo().IsGenericType;
 
@@ -78,7 +78,7 @@ namespace ElasticLinq.Mapping
         /// <remarks>
         /// This is likely System.ComponentModel.DataAnnotations.RequiredAttribute but doesn't have to be.
         /// </remarks>
-        public static Func<TypeInfo, PropertyInfo> RequiredProperty = t =>
+        static readonly Func<TypeInfo, PropertyInfo> requiredProperty = t =>
             t.DeclaredProperties.FirstOrDefault(f => f.CustomAttributes.Any(a => a.AttributeType.Name == "RequiredAttribute"));
     }
 }
