d31a590c43f54ec8f96623419f044994334984bd
Damien Guard <damieng@gmail.com>
2017-01-24 20:20:28 -0800

    Break on error handler for integration tests.

---
+++ Source/ElasticLINQ.IntegrationTest/BreakOnInvalidQueryConnection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using ElasticLinq.Logging;
+using ElasticLinq.Request;
+using ElasticLinq.Response.Model;
+
+namespace ElasticLinq.IntegrationTest
+{
+    class BreakOnInvalidQueryConnection : ElasticConnection
+    {
+        public BreakOnInvalidQueryConnection(Uri endpoint, string userName = null, string password = null, TimeSpan? timeout = null, string index = null, ElasticConnectionOptions options = null)
+            : base(endpoint, userName, password, timeout, index, options)
+        {
+        }
+
+        public override async Task<ElasticResponse> SearchAsync(string body, SearchRequest searchRequest, CancellationToken token, ILog log)
+        {
+            try
+            {
+                return await base.SearchAsync(body, searchRequest, token, log);
+            }
+            catch (Exception ex)
+            {
+                var url = GetSearchUri(searchRequest);
+                Debugger.Break();
+                throw;
+            }
+        }
+    }
+}
