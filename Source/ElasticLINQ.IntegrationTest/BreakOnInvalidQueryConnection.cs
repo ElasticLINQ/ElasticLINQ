1dc8e65f1805b402d83c1f63986b77abfc3e40fd
Damien Guard <damieng@gmail.com>
2017-01-24 20:39:55 -0800

    Clean up BreakOnInvalid connection handler.

--- Source/ElasticLINQ.IntegrationTest/BreakOnInvalidQueryConnection.cs
+++ Source/ElasticLINQ.IntegrationTest/BreakOnInvalidQueryConnection.cs
@@ -21,9 +21,8 @@ namespace ElasticLinq.IntegrationTest
             {
                 return await base.SearchAsync(body, searchRequest, token, log);
             }
-            catch (Exception ex)
+            catch (Exception)
             {
-                var url = GetSearchUri(searchRequest);
                 Debugger.Break();
                 throw;
             }
