fc2484424ab17971a6b7520d764355a38f0c7c2f
Damien Guard <damieng@gmail.com>
2017-03-29 22:38:30 -0700

    Bump GlobalAssemblyInfo.cs for 1.4.3

--- Source/GlobalAssemblyInfo.cs
+++ Source/GlobalAssemblyInfo.cs
@@ -2,8 +2,8 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("ElasticLINQ")]
-[assembly: AssemblyCopyright("Copyright (C) 2013-2016 CenturyLink, Inc.")]
+[assembly: AssemblyCopyright("Copyright (C) 2013-2017 CenturyLink, Inc.")]
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.4.2.0")]
-[assembly: AssemblyFileVersion("1.4.2.0")]
+[assembly: AssemblyVersion("1.4.3.0")]
+[assembly: AssemblyFileVersion("1.4.3.0")]
