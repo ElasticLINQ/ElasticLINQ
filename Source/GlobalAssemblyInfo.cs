3f9f3008f8468fb8eae84134d272f9937bdcdc96
Damien Guard <damieng@gmail.com>
2018-08-09 10:21:53 -0700

    Update copyright notice to 2018

--- Source/GlobalAssemblyInfo.cs
+++ Source/GlobalAssemblyInfo.cs
@@ -2,7 +2,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("ElasticLINQ")]
-[assembly: AssemblyCopyright("Copyright (C) 2013-2017 CenturyLink, Inc.")]
+[assembly: AssemblyCopyright("Copyright (C) 2013-2018 CenturyLink, Inc.")]
 
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.4.3.0")]
